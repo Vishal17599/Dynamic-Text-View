6af28fbd9d33dfd2780d1f6bb491994b373212bb
Vishal17599 <Vishal.parmar17599@gmail.com>
2020-02-12 23:53:40 +0530

    first commit

---
+++ app/src/main/java/com/example/android/dynamicedittext/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.android.dynamicedittext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Layout;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.StaticLayout;
+import android.text.TextPaint;
+import android.text.style.AbsoluteSizeSpan;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
